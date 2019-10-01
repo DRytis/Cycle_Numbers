53c16916b1c6e1e5e13e367fb46cd2b46be11230
Rytis Danyla <rytisdanyla@gmail.com>
2019-10-01 21:13:23 +0300

    commitas

--- src/days/first/CycleNumbers.java
+++ src/days/first/CycleNumbers.java
@@ -11,20 +11,26 @@ public class CycleNumbers {
             System.out.println("Enter number");
             try {
                 int numb = scanner.nextInt();
-
-                String numbToStr = Integer.toString(numb);
-                char numbArr[] = numbToStr.toCharArray();
-                int numbX2 = numb * 2;
-                String numbToStrX2 = Integer.toString(numbX2);
-                char numbArrX2[] = numbToStrX2.toCharArray();
-                Arrays.sort(numbArr);
-                Arrays.sort(numbArrX2);
-                if (Arrays.equals(numbArr, numbArrX2)) {
-                    System.out.println("Magic");
-                } else {
-                    System.out.printf("Not magic");
+                if (numb != 0) {
+                    String numbToStr = Integer.toString(numb);
+                    char numbArr[] = numbToStr.toCharArray();
+                    int numbX2 = numb * 2;
+                    String numbToStrX2 = Integer.toString(numbX2);
+                    char numbArrX2[] = numbToStrX2.toCharArray();
+                    Arrays.sort(numbArr);
+                    Arrays.sort(numbArrX2);
+                    if (Arrays.equals(numbArr, numbArrX2)) {
+                        System.out.println("Magic");
+                    } else {
+                        System.out.printf("Not magic");
+                    }
+                    break;
                 }
-                break;
+                else {
+                    System.out.println("Enter correct number.");
+                    scanner.nextLine();
+                }
+
             } catch (InputMismatchException e) {
                 System.out.println("Enter correct number.");
                 scanner.nextLine();
